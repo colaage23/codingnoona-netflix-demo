c74b88bec5c7da6939ff96ec62182db295907b03
KGB <colaage23@gmail.com>
2026-04-11 22:29:24 +0900

    feat: banner 추가

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../utils/api";
+
+const fetchPopularMovies = async () => {
+  return await api.get("/movie/popular");
+};
+
+export const usePopularMoviesQuery = () => {
+  return useQuery({
+    queryKey: ["movie-popular"],
+    queryFn: fetchPopularMovies,
+    select: (response) => response.data,
+  });
+};
